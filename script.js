815ab2cdd9a9041c7a70c01fff3b722b7c809884
jakoozi <jamescallum2384@gmail.com>
2021-09-08 11:15:22 +0100

    added button disable

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 
 function EmailSender(){
+
+    document.getElementById("email-submit-button").disabled  = true;
  
     var tempParams ={
         // from_name : document.getElementById("fromName").value,
@@ -17,15 +19,20 @@ function EmailSender(){
     }, function(error) {
       AfterEmailSentUnSuccessfully(error)
     });
+    
 }
 
 function AfterEmailSentSuccessfully(response){
     console.log('SUCCESS!', response);
     document.getElementById("delivery-status").innerHTML = "Successfull !!!";
+    document.getElementById("delivery-status").style.color = "Green";
+    document.getElementById("email-submit-button").disabled  = false;
     Swal("Done!", response.text, "success");
 }
 function AfterEmailSentUnSuccessfully(error){
     console.log('FAILED...', error);
     document.getElementById("delivery-status").innerHTML = "Error !!!";
+    document.getElementById("delivery-status").style.color = "red";
+    document.getElementById("email-submit-button").disabled  = false;
     Swal("Oops!", error, "error");
 }
